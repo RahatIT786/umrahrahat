096bc690f508615fc2557798fc78792ac562c270
abu <rahatit486@gmail.com>
2024-12-24 17:00:32 +0530

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Livewire\UserHome;
 //     return view('index');
 // });
 
-<<<<<<< HEAD
+
 Route::get('/',UserHome::class)->name('user.home');
 Route::get('/about',UserHome::class)->name('user.about');
-=======
+
 Route::get('/',UserHome::class)->name('user.layouts.app');
->>>>>>> 58ed0575dd483bd89de4237e38bd9835d7217530
+
