6436a6633088034ea7c0e4653416e8725751ab1f
RahatIT786 <rahatit486@gmail.com>
2024-12-28 10:41:15 +0530

    CRU opration done for visa componend by karthi

---
+++ app/Models/VisaDetail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class VisaDetail extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'visa_type',
+        'documents_required',
+        'processing_time',
+        'price',
+        'file_path',
+    ];
+}
