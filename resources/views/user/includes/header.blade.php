ecfc9f6a60bcaffa0b2fc03bb08090ee2dfa20bb
RahatIT786 <rahatit486@gmail.com>
2025-01-06 17:46:53 +0530

    saudi visa modification by karthi

--- resources/views/user/includes/header.blade.php
+++ resources/views/user/includes/header.blade.php
@@ -7,11 +7,7 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="https://templatekit.rometheme.pro/prayer/xmlrpc.php" />
     <title>Homepage &#8211; Prayer</title>
-<<<<<<< HEAD
     <meta name='robots' content='max-image-preview:large' />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-=======
-    <meta name='robots' content='max-image-preview:large' />
->>>>>>> f73d93055904dae33332d5e198062698bd290b36
